21511f0921b05854e0ce6be434e4a06e6528d6c9
Daniel Díaz <dhelta.diaz@gmail.com>
2013-06-24 19:14:12 +0200

    Export list for PathBuilder.hs and introduction to documentation.

--- Text/LaTeX/Packages/TikZ/PathBuilder.hs
+++ Text/LaTeX/Packages/TikZ/PathBuilder.hs
@@ -1,8 +1,34 @@
 
+-- | This module provides a monadic interface to build 'TPath' values.
+--   It does so using 'PathBuilder's. The construction of a 'PathBuilder'
+--   is equivalent to the construction of a 'TPath' by hand, but with
+--   a possibly more convenient syntax.
+--
+--   For example, this path corresponds to a triangle:
+--
+-- > trianglePath :: TPath
+-- > trianglePath = bpath (pointAtXY (-1) 0) $ do
+-- >    line $ pointAtXY 1 0
+-- >    line $ pointAtXY 0 1
+-- >    pcycle
+--
+--   The equivalent syntax created by hand would be:
+--
+-- > trianglePath :: TPath
+-- > trianglePath = Cycle (Line (Line (Start (pointAtXY (-1) 0)) (pointAtXY 1 0)) (pointAtXY 0 1))
+--
 module Text.LaTeX.Packages.TikZ.PathBuilder (
    -- * Path builder
    PathBuilder
-  ) where
+ , bpath
+   -- * Builder functions
+ , line
+ , pcycle
+ , rectangle
+ , circle
+ , ellipse
+ , grid
+   ) where
 
 import Text.LaTeX.Packages.TikZ.Syntax
 import Control.Monad.Trans.State
@@ -30,8 +56,8 @@ instance Monad PathBuilder where
 applyToPath :: (TPath -> TPath) -> PathBuilder ()
 applyToPath f = PB $ modify $ \ps -> ps { currentPath = f (currentPath ps) }
 
-tcycle :: PathBuilder ()
-tcycle = applyToPath Cycle
+pcycle :: PathBuilder ()
+pcycle = applyToPath Cycle
 
 line :: TPoint -> PathBuilder ()
 line p = applyToPath $ (`Line`p)
@@ -48,7 +74,7 @@ ellipse r1 r2 = applyToPath $ \x -> Ellipse x r1 r2
 grid :: [GridOption] -> TPoint -> PathBuilder ()
 grid xs p = applyToPath $ \x -> Grid x xs p
 
--- | Build a path using a 'PathBuilder' and a /starting point/.
-bpath :: PathBuilder a -> TPoint -> TPath
-bpath pb p = currentPath $ execState (pathBuilder pb) (PS $ Start p)
+-- | Build a path using a /starting point/ and a 'PathBuilder'.
+bpath :: TPoint -> PathBuilder a -> TPath
+bpath p pb = currentPath $ execState (pathBuilder pb) (PS $ Start p)
 
