d11abf932ec2eabaaa529d7d3c9b592f8dc9922d
Daniel Díaz <dhelta.diaz@gmail.com>
2013-07-07 17:00:27 +0200

    New package: fancyhdr.

---
+++ Text/LaTeX/Packages/Fancyhdr.hs
@@ -0,0 +1,109 @@
+
+-- | This package provides extensive control of page headers and footers.
+--
+-- CTAN page for fancyhdr: <http://www.ctan.org/pkg/fancyhdr>.
+module Text.LaTeX.Packages.Fancyhdr (
+   -- * fancyhdr package
+   fancyhdr
+   -- * Simple interface
+ , HdrSettings (..)
+ , applyHdrSettings
+   -- * Raw interface
+ , fancy
+ , lhead, chead, rhead
+ , lfoot, cfoot, rfoot
+ , renewheadrulewidth
+ , renewfootrulewidth
+   ) where
+
+import Text.LaTeX.Base.Syntax
+import Text.LaTeX.Base.Class
+import Text.LaTeX.Base.Types
+import Text.LaTeX.Base.Commands
+import Text.LaTeX.Base.Texy
+
+fancyhdr :: PackageName
+fancyhdr = "fancyhdr"
+
+-- Header and footer settigns
+
+data HdrSettings = HdrSettings
+ { leftHeader    :: LaTeX
+ , centerHeader  :: LaTeX
+ , rightHeader   :: LaTeX
+ , leftFooter    :: LaTeX
+ , centerFooter  :: LaTeX
+ , rightFooter   :: LaTeX
+ , headRuleWidth :: Measure
+ , footRuleWidth :: Measure
+   } deriving (Eq,Show)
+
+defaultHdrSettings :: HdrSettings
+defaultHdrSettings =
+  HdrSettings
+   { leftHeader    = mempty
+   , centerHeader  = mempty
+   , rightHeader   = mempty
+   , leftFooter    = mempty
+   , centerFooter  = thePage
+   , rightFooter   = mempty
+   , headRuleWidth = Pt 0.4
+   , footRuleWidth = Pt 0
+     }
+
+applyHdrSettings :: LaTeXC l => HdrSettings -> l
+applyHdrSettings hs =
+    usepackage [] fancyhdr
+ <> pagestyle fancy
+ <> fromLaTeX (lhead $   leftHeader hs)
+ <> fromLaTeX (chead $ centerHeader hs)
+ <> fromLaTeX (rhead $  rightHeader hs)
+ <> fromLaTeX (lfoot $   leftFooter hs)
+ <> fromLaTeX (cfoot $ centerFooter hs)
+ <> fromLaTeX (rfoot $  rightFooter hs)
+ <> renewheadrulewidth (headRuleWidth hs)
+ <> renewfootrulewidth (footRuleWidth hs)
+
+-- Raw interface
+
+-- | Page style of the 'fancyhdr' package.
+fancy :: PageStyle
+fancy = "fancy"
+
+-- | Set the left header.
+lhead :: LaTeXC l => l -> l
+lhead = comm1 "lhead"
+
+-- | Set the center header.
+chead :: LaTeXC l => l -> l
+chead = comm1 "chead"
+
+-- | Set the right header.
+rhead :: LaTeXC l => l -> l
+rhead = comm1 "rhead"
+
+-- | Set the left footer.
+lfoot :: LaTeXC l => l -> l
+lfoot = comm1 "lfoot"
+
+-- | Set the center footer.
+cfoot :: LaTeXC l => l -> l
+cfoot = comm1 "cfoot"
+
+-- | Set the right footer.
+rfoot :: LaTeXC l => l -> l
+rfoot = comm1 "rfoot"
+
+-- | Set the @headrulewidth@ attribute.
+renewheadrulewidth :: LaTeXC l => Measure -> l
+renewheadrulewidth m = fromLaTeX $
+  TeXComm "renewcommand" [ FixArg $ TeXCommS "headrulewidth"
+                         , FixArg $ texy m
+                           ]
+
+-- | Set the @footrulewidth@ attribute.
+renewfootrulewidth :: LaTeXC l => Measure -> l
+renewfootrulewidth m = fromLaTeX $
+  TeXComm "renewcommand" [ FixArg $ TeXCommS "footrulewidth"
+                         , FixArg $ texy m
+                           ]
