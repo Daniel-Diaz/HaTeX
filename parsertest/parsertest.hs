137921159f71826f3de0385bf2b8a9bc81838e33
Daniel Díaz <dhelta.diaz@gmail.com>
2013-10-24 12:50:45 +0200

    Added parser tester with a couple of examples.

---
+++ parsertest/parsertest.hs
@@ -0,0 +1,26 @@
+
+import Text.LaTeX
+import Text.LaTeX.Base.Parser
+import qualified Data.Text.IO as T
+
+testNumbers :: [Int]
+testNumbers = [1 .. 2]
+
+testFile :: Int -> IO Bool
+testFile i = do
+  putStr $ "Parsing example " ++ show i ++ "... "
+  t <- T.readFile $ "example" ++ show i ++ ".tex"
+  case parseLaTeX t of
+    Left err -> do putStrLn "Failed."
+                   putStrLn $ "The error was: " ++ err
+                   return False
+    Right _  -> putStrLn "Succeed." >> return True
+  
+main :: IO ()
+main = do
+  putStrLn "Running Parser Test..."
+  bs <- mapM testFile testNumbers
+  let b = and bs
+  putStrLn $ "Parser Test Passed: " ++ show b
+  if b then return ()
+       else putStrLn $ "Number of errors: " ++ show (length $ filter (==False) bs)
