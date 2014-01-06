a304f99adf401a79d31cd88624a5594270e2e045
Daniel Díaz <dhelta.diaz@gmail.com>
2014-01-06 13:32:34 +0100

    Added starting point for Geometry package implementation.

---
+++ Text/LaTeX/Packages/Geometry.hs
@@ -0,0 +1,16 @@
+
+module Text.LaTeX.Packages.Geometry (
+    -- * Geometry package
+    geometry
+  ) where
+
+import Text.LaTeX
+
+-- | Geometry package. Use it to import it like this:
+--
+-- > usepackage [] geometry
+--
+geometry :: PackageName
+geometry = "geometry"
+
+
