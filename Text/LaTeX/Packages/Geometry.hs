5d1a3bbf49e19c01e85faa9e2e4967282322000b
Daniel Díaz <dhelta.diaz@gmail.com>
2014-01-30 18:02:45 +0100

    Added minimal functionality to the Geometry package

--- Text/LaTeX/Packages/Geometry.hs
+++ Text/LaTeX/Packages/Geometry.hs
@@ -1,10 +1,17 @@
 
+{-# LANGUAGE OverloadedStrings #-}
+
 module Text.LaTeX.Packages.Geometry (
     -- * Geometry package
     geometry
+    -- * Geometry options
+  , GeometryOption (..)
+  , applyGeometry
   ) where
 
 import Text.LaTeX
+import Text.LaTeX.Base.Syntax
+import Text.LaTeX.Base.Class
 
 -- | Geometry package. Use it to import it like this:
 --
@@ -13,4 +20,22 @@ import Text.LaTeX
 geometry :: PackageName
 geometry = "geometry"
 
+-- | Options of the geometry package.
+data GeometryOption = 
+    GHeight Measure
+  | GWidth  Measure
+  | GPaper PaperType
+  | GCentered
+    deriving Show
 
+renderOption :: Render a => Text -> a -> Text
+renderOption t x = t <> "=" <> render x
+
+instance Render GeometryOption where
+  render (GHeight m) = renderOption "height" m
+  render (GWidth  m) = renderOption "width"  m
+  render (GPaper  p) = render (Paper p)
+  render GCentered   = "centered"
+
+applyGeometry :: LaTeXC l => [GeometryOption] -> l
+applyGeometry opts = fromLaTeX $ TeXComm "geometry" [FixArg $ raw $ renderCommas opts]
