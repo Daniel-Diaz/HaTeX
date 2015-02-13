dcbf50e9f88eaff82330f78551be23d283c6d365
Alex Rozenshteyn <rpglover64@gmail.com>
2015-02-13 10:21:01 -0500

    Tweak failing test.
    The "render . parse = id" property no longer holds. Well, it never
    held (see #48). Rendering a document that was TeXRaw may produce a
    string with spacing that will change after one "render . parse" cycle.

--- test/Main.hs
+++ test/Main.hs
@@ -19,7 +19,7 @@ main = defaultMain $ testGroup "HaTeX"
     ]
   , testGroup "Parser"
     [ QC.testProperty "render . parse = id" $
-         \l -> let t = render (l :: LaTeX)
+         \l -> let t = render . parse . render (l :: LaTeX)
                in  fmap render (parseLaTeX t) == Right t
     ]
   ]
