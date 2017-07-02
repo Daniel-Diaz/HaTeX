1e22f16bbdab31d8db87bee4dcbc03cb33ca1ea9
Daniel Casanueva <dhelta.diaz@gmail.com>
2017-07-02 19:21:40 +0200

    Improve arbitraryChar to make tests more reliable.

--- Text/LaTeX/Base/Syntax.hs
+++ Text/LaTeX/Base/Syntax.hs
@@ -277,7 +277,7 @@ arbitraryChar :: Gen Char
 arbitraryChar = elements $
      ['A'..'Z']
   ++ ['a'..'z']
-  ++ "\n-+*/!\"$%&(){}^_.,:;'#@<>? "
+  ++ "\n-+*/!\"().,:;'@<>? "
 
 -- | Utility for the instance of 'LaTeX' to 'Arbitrary'.
 --   We generate a short sequence of characters and
