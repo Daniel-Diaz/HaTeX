ecf015a8c2bb2233dca239d8d799004c65b27259
Daniel Casanueva <dhelta.diaz@gmail.com>
2019-05-21 23:09:27 +0200

    Migrate to prettyprinter and version bump.

--- Text/LaTeX/Base/Pretty.hs
+++ Text/LaTeX/Base/Pretty.hs
@@ -13,26 +13,32 @@ module Text.LaTeX.Base.Pretty (
 
 import Text.LaTeX.Base.Syntax
 import Text.LaTeX.Base.Render
-import Text.PrettyPrint.Free
-  ( Doc, text, char
+import Data.Text.Prettyprint.Doc
+  ( Doc, pretty
   , backslash, line, softline, hardline
   , braces, brackets
   , indent, align, vsep
   , list, encloseSep
-  , renderSmart, displayS
+  , LayoutOptions (..)
+  , PageWidth (..)
+  , layoutSmart
     )
+import Data.Text.Prettyprint.Doc.Render.String (renderString)
 import Data.Text (unpack,lines)
 #if !MIN_VERSION_base(4,8,0)
 import Data.Monoid (mconcat,mempty)
 #endif
 
+text :: Text -> Doc ann
+text = pretty
+
 -- | This function transforms a value of type 'LaTeX' to a 'Doc'.
 --   You can then choose how to print this 'Doc' value using
 --   the function from the "Text.PrettyPrint.Free" module.
 docLaTeX :: LaTeX -> Doc ()
-docLaTeX (TeXRaw t) = text $ unpack t
-docLaTeX (TeXComm n as) = backslash <> text n <> align (mconcat (fmap docTeXArg as)) <> softline
-docLaTeX (TeXCommS n) = backslash <> text n <> softline
+docLaTeX (TeXRaw t) = pretty $ unpack t
+docLaTeX (TeXComm n as) = backslash <> pretty n <> align (mconcat (fmap docTeXArg as)) <> softline
+docLaTeX (TeXCommS n) = backslash <> pretty n <> softline
 docLaTeX (TeXEnv n as b) =
   let a = FixArg $ fromString n
   in  mconcat
@@ -51,13 +57,13 @@ docLaTeX (TeXMath t b) =
           DoubleDollar -> ("$$","$$")
   in  text l <> docLaTeX b <> text r
 docLaTeX (TeXLineBreak m b) =
-  text "\\\\" <> maybe mempty (brackets . text . unpack . render) m <> ( if b then text "*" else mempty )
+  text "\\\\" <> maybe mempty (brackets . pretty . unpack . render) m <> ( if b then text "*" else mempty )
 docLaTeX (TeXBraces b) = braces $ docLaTeX b
 docLaTeX (TeXComment t) =
   let ls = Data.Text.lines t
   in  if null ls
-         then char '%' <> hardline
-         else (align $ vsep $ fmap (text . ("% "++) . unpack) ls) <> hardline
+         then pretty '%' <> hardline
+         else (align $ vsep $ fmap (pretty . ("% "++) . unpack) ls) <> hardline
 docLaTeX (TeXSeq l1 l2) = docLaTeX l1 <> docLaTeX l2
 docLaTeX TeXEmpty = mempty
 
@@ -68,9 +74,9 @@ docTeXArg (MOptArg ls) =
   if null ls then mempty
              else list $ fmap docLaTeX ls
 docTeXArg (SymArg l) = docTeXArg $ MSymArg [l]
-docTeXArg (MSymArg ls) = encloseSep (char '<') (char '>') (char ',') $ fmap docLaTeX ls
+docTeXArg (MSymArg ls) = encloseSep (pretty '<') (pretty '>') (pretty ',') $ fmap docLaTeX ls
 docTeXArg (ParArg l) = docTeXArg $ MParArg [l]
-docTeXArg (MParArg ls) = encloseSep (char '(') (char ')') (char ',') $ fmap docLaTeX ls
+docTeXArg (MParArg ls) = encloseSep (pretty '(') (pretty ')') (pretty ',') $ fmap docLaTeX ls
 
 -- | Pretty print a 'LaTeX' value. It produces a more human-friendly output than 'render'.
 --
@@ -81,4 +87,7 @@ docTeXArg (MParArg ls) = encloseSep (char '(') (char ')') (char ',') $ fmap docL
 --   You should use 'renderFile' unless you really need to read the LaTeX file.
 --
 prettyLaTeX :: LaTeX -> String
-prettyLaTeX l = displayS (renderSmart 60 (docLaTeX l)) []
+prettyLaTeX = renderString . layoutSmart layout . docLaTeX
+
+layout :: LayoutOptions
+layout = LayoutOptions $ AvailablePerLine 60 1
