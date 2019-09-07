eaeeba9347ec73e3deae2d27a90e13b0324572d2
Willem Van Onsem <vanonsem.willem@gmail.com>
2019-09-07 23:14:34 +0200

    added use management of the acronym, some basic documentation

--- Text/LaTeX/Packages/Acronym.hs
+++ Text/LaTeX/Packages/Acronym.hs
@@ -8,25 +8,18 @@ module Text.LaTeX.Packages.Acronym
  ( -- * Acronym package
    pacronym
    -- * Package options
- , footnote
- , nohyperlinks
- , printonlyused
- , withpage
- , smaller
- , dua
- , nolist
+ , footnote, nohyperlinks, printonlyused, withpage, smaller, dua, nolist
  -- * Types
  , Acronym(..)
  -- functions
- , ac, acf, acs, acl, acp, acfp, acsp, aclp, acfi, acsu, aclu
- , iac, iac2
- , ac', acf', acs', acl', acp', acfp', acsp', aclp', acfi', acsu', aclu'
- , iac', iac2'
+ , ac, acf, acs, acl, acp, acfp, acsp, aclp, acfi, acsu, aclu, iac, iac2
+ , ac', acf', acs', acl', acp', acfp', acsp', aclp', acfi', acsu', aclu', iac', iac2'
+ , acresetall, acused, 
    ) where
 
 import Data.String(IsString(fromString))
 
-import Text.LaTeX.Base.Class(LaTeXC, comm1)
+import Text.LaTeX.Base.Class(LaTeXC, comm0, comm1)
 import Text.LaTeX.Base.Types(PackageName)
 
 -- | The 'pacronym' package.
@@ -79,83 +72,108 @@ newtype Acronym = Acronym { acronymLabel :: String }
 _acronymLabel :: IsString s => Acronym -> s
 _acronymLabel = fromString . acronymLabel
 
-_acronymLabel' :: LaTeXC l => String -> Acronym -> l
-_acronymLabel' = (. _acronymLabel) . comm1
+_acronymC1 :: LaTeXC l => String -> Acronym -> l
+_acronymC1 = (. _acronymLabel) . comm1
 
+-- | Enter an acronym inside the text. The first time the acronym is used, it
+--   will specify the full name, and between parenthesis, the short name. If you
+--   specified the `footnote` option, it will print the short name, and add a
+--   footnote with the long name. The next time, only the short time is printed.
 ac :: LaTeXC l => Acronym -> l
-ac = _acronymLabel' "ac"
+ac = _acronymC1 "ac"
 
+-- | You can use this command to later in the text again print the full name of
+--   the acronym, this stands for "full acronym", it always prints the full
+--   name, and the acronym between parenthesis.
 acf :: LaTeXC l => Acronym -> l
-acf = _acronymLabel' "acf"
+acf = _acronymC1 "acf"
 
+-- | This will enter the short version of the acronym inside the text.
 acs :: LaTeXC l => Acronym -> l
-acs = _acronymLabel' "acs"
+acs = _acronymC1 "acs"
 
+-- | This will enter the expanded version of the acronym in the text, without
+--   mentioning the acronym between parenthesis.
 acl :: LaTeXC l => Acronym -> l
-acl = _acronymLabel' "acl"
+acl = _acronymC1 "acl"
 
+-- | This works the same way as `ac`, except that it will make the short and the
+--   long forms into plurals.
 acp :: LaTeXC l => Acronym -> l
-acp = _acronymLabel' "acp"
+acp = _acronymC1 "acp"
 
+-- | This works the same way as `acf`, except tah it will make the short and
+--   long forms into plurals.
 acfp :: LaTeXC l => Acronym -> l
-acfp = _acronymLabel' "acfp"
+acfp = _acronymC1 "acfp"
 
 acsp :: LaTeXC l => Acronym -> l
-acsp = _acronymLabel' "acsp"
+acsp = _acronymC1 "acsp"
 
 aclp :: LaTeXC l => Acronym -> l
-aclp = _acronymLabel' "aclp"
+aclp = _acronymC1 "aclp"
 
 acfi :: LaTeXC l => Acronym -> l
-acfi = _acronymLabel' "acfi"
+acfi = _acronymC1 "acfi"
 
 acsu :: LaTeXC l => Acronym -> l
-acsu = _acronymLabel' "acsu"
+acsu = _acronymC1 "acsu"
 
 aclu :: LaTeXC l => Acronym -> l
-aclu = _acronymLabel' "aclu"
+aclu = _acronymC1 "aclu"
 
 iac :: LaTeXC l => Acronym -> l
-iac = _acronymLabel' "iac"
+iac = _acronymC1 "iac"
 
 iac2 :: LaTeXC l => Acronym -> l
-iac2 = _acronymLabel' "Iac"
+iac2 = _acronymC1 "Iac"
 
 ac' :: LaTeXC l => Acronym -> l
-ac' = _acronymLabel' "ac*"
+ac' = _acronymC1 "ac*"
 
 acf' :: LaTeXC l => Acronym -> l
-acf' = _acronymLabel' "acf*"
+acf' = _acronymC1 "acf*"
 
 acs' :: LaTeXC l => Acronym -> l
-acs' = _acronymLabel' "acs*"
+acs' = _acronymC1 "acs*"
 
 acl' :: LaTeXC l => Acronym -> l
-acl' = _acronymLabel' "acl*"
+acl' = _acronymC1 "acl*"
 
 acp' :: LaTeXC l => Acronym -> l
-acp' = _acronymLabel' "acp*"
+acp' = _acronymC1 "acp*"
 
 acfp' :: LaTeXC l => Acronym -> l
-acfp' = _acronymLabel' "acfp*"
+acfp' = _acronymC1 "acfp*"
 
 acsp' :: LaTeXC l => Acronym -> l
-acsp' = _acronymLabel' "acsp*"
+acsp' = _acronymC1 "acsp*"
 
 aclp' :: LaTeXC l => Acronym -> l
-aclp' = _acronymLabel' "aclp*"
+aclp' = _acronymC1 "aclp*"
 
 acfi' :: LaTeXC l => Acronym -> l
-acfi' = _acronymLabel' "acfi*"
+acfi' = _acronymC1 "acfi*"
 
 acsu' :: LaTeXC l => Acronym -> l
-acsu' = _acronymLabel' "acsu*"
+acsu' = _acronymC1 "acsu*"
 
 aclu' :: LaTeXC l => Acronym -> l
-aclu' = _acronymLabel' "aclu*"
+aclu' = _acronymC1 "aclu*"
 
 iac' :: LaTeXC l => Acronym -> l
-iac' = _acronymLabel' "iac*"
+iac' = _acronymC1 "iac*"
 
 iac2' :: LaTeXC l => Acronym -> l
-iac2' = _acronymLabel' "Iac*"
+iac2' = _acronymC1 "Iac*"
+
+-- | The memory of the marco `ac` is flushed, afterwards, `ac` will print the
+--   full name of any acronym, and the acronym within parenthesis.
+acresetall :: LaTeXC l => l
+acresetall = comm0 "acresetall"
+
+-- | Marks an acronym as useed, as if it has been called with `ac`, but without
+--   printing anything. In the future, only the short form of the acronym will
+--   be printed. `acresetall` undoes this.
+acused :: LaTeXC l => Acronym -> l
+acused = _acronymC1 "acused"
